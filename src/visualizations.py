1154c16424e579ae2cdee02000dd2722a292c14e
Abhijith <abhijith.thailur@gmail.com>
2025-04-25 11:25:28 +0530

    Uncommented the code

--- src/visualizations.py
+++ src/visualizations.py
@@ -7,39 +7,39 @@ file_path = '/Users/ABHIJN/Downloads/Global_AI_Content_Impact_Dataset.csv'
 df = pd.read_csv(file_path)
 
 # 1. Bar chart for Top 5 Countries by AI Adoption Rate
-# top5 = df.groupby('Country')['AI Adoption Rate (%)'].mean().sort_values(ascending=False).head(5)
-#
-# # Plot
-# plt.figure(figsize=(10,6))
-# sns.barplot(x=top5.index, y=top5.values, color='skyblue')  # <-- change here
-# plt.title('Top 5 Countries by Average AI Adoption Rate (%)')
-# plt.ylabel('AI Adoption Rate (%)')
-# plt.xlabel('Country')
-# plt.xticks(rotation=45)
-# plt.tight_layout()
-#
-# # Save chart as an image
-# plt.savefig('top5_countries_ai_adoption.png')
-# plt.show()
+top5 = df.groupby('Country')['AI Adoption Rate (%)'].mean().sort_values(ascending=False).head(5)
 
-# Group data: Average Revenue Increase per Year
-# revenue_by_year = df.groupby('Year')['Revenue Increase Due to AI (%)'].mean().reset_index()
-#
-# # Plot
-# plt.figure(figsize=(10,6))
-# sns.lineplot(x='Year', y='Revenue Increase Due to AI (%)', data=revenue_by_year, marker='o', color='green')
-# plt.title('Average Revenue Increase Due to AI Over Years')
-# plt.xlabel('Year')
-# plt.ylabel('Average Revenue Increase (%)')
-# plt.grid(True)
-# plt.tight_layout()
-#
-# # Save the figure
-# plt.savefig('revenue_increase_trend.png')
-# plt.show()
+# Plot
+plt.figure(figsize=(10,6))
+sns.barplot(x=top5.index, y=top5.values, color='skyblue')  # <-- change here
+plt.title('Top 5 Countries by Average AI Adoption Rate (%)')
+plt.ylabel('AI Adoption Rate (%)')
+plt.xlabel('Country')
+plt.xticks(rotation=45)
+plt.tight_layout()
 
-# Show all column names
-# print(df.columns.tolist())
+# Save chart as an image
+plt.savefig('top5_countries_ai_adoption.png')
+plt.show()
+
+Group data: Average Revenue Increase per Year
+revenue_by_year = df.groupby('Year')['Revenue Increase Due to AI (%)'].mean().reset_index()
+
+# Plot
+plt.figure(figsize=(10,6))
+sns.lineplot(x='Year', y='Revenue Increase Due to AI (%)', data=revenue_by_year, marker='o', color='green')
+plt.title('Average Revenue Increase Due to AI Over Years')
+plt.xlabel('Year')
+plt.ylabel('Average Revenue Increase (%)')
+plt.grid(True)
+plt.tight_layout()
+
+# Save the figure
+plt.savefig('revenue_increase_trend.png')
+plt.show()
+
+#Show all column names
+print(df.columns.tolist())
 
 # SAFELY fix column names (optional but useful)
 df.columns = df.columns.str.strip()  # Remove any accidental spaces or newlines
